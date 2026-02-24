1e8f8d0c7b4905104a4e9f4f80e79577557fedbd
Alicia McGregor <156120561+AliciaMcG@users.noreply.github.com>
2026-02-24 10:26:01 -0500

    Ritual 2 Behaviour Scripts

---
+++ WhatRemains/Assets/Scripts/puzzleBehaviour/ingredientJar.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ingredientJar : MonoBehaviour
+{
+    /// <summary>
+    /// Holds code for:
+    /// 
+    ///   Ingredient id to check validity
+    ///   correct ingredients:
+    ///     snake skin, jarID = 0
+    ///     mushrooms,  jarID = 1
+    ///     teeth,      jarID = 2
+    ///     blood,      jarID = 3
+    ///     other,      jarID = 4
+    ///   
+    /// </summary>
+    /// 
+
+    public int id;
+    
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
--- WhatRemains/Assets/Scripts/puzzleBehaviour/puzzle2Behaviour.cs
+++ WhatRemains/Assets/Scripts/puzzleBehaviour/puzzle2Behaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -5,12 +6,27 @@ using UnityEngine;
 /// 
 ///   Puzzle index 1
 ///   
+///   correct ingredients:
+///     snake skin, jarID = 0
+///     mushrooms,  jarID = 1
+///     teeth,      jarID = 2
+///     blood,      jarID = 3
+///     other,      jarID = 4
 /// </summary>
 
 public class puzzle2Behaviour : MonoBehaviour
 {
     ///////////////////////////////////////////////////////////      VARS      ////////////////////////////////////////////////////////////////////////////////
+    [Header("Objects")]
+    gameplayBase puzzleManager;
+    public GameObject cauldron;
 
+    [Header("Recipe")]
+    public List<int> requiredIngredients = new List<int> { 0, 1, 2, 3 };
+    public List<int> currentIngredients;
+    bool isSpoiled;
+
+    //[Header("Lists")]
 
     ///////////////////////////////////////////////////////////      LOOPSS      ////////////////////////////////////////////////////////////////////////////////
     private void Awake()
@@ -19,12 +35,11 @@ public class puzzle2Behaviour : MonoBehaviour
 
     void Start()
     {
-
+        isSpoiled = false;
     }
 
     void Update()
     {
-
     }
 
     private void FixedUpdate()
@@ -34,4 +49,55 @@ public class puzzle2Behaviour : MonoBehaviour
 
     ///////////////////////////////////////////////////////////      FUNCTIONS      ////////////////////////////////////////////////////////////////////////////////
     ///
+    public void addIngredient(GameObject ingredient)
+    {
+        if (isSpoiled == true)
+        {
+            return;
+        }
+
+        ingredientJar jar = ingredient.GetComponent<ingredientJar>();
+
+        if (jar == null)
+        {
+            return;
+        }
+
+        // compare jar type to required jar types
+        int jarID = jar.id;
+
+        if (!requiredIngredients.Contains(jarID))
+        {
+            spoilMixture();
+            return;
+        }
+
+        if (!currentIngredients.Contains(jarID))
+        {
+            currentIngredients.Add(jarID);
+            checkMixture();
+        }
+    }
+
+    void checkMixture()
+    {
+        // play sound effect for correct ingredient
+        
+        if (currentIngredients.Count == requiredIngredients.Count)
+        {
+            puzzleManager.completePuzzle(1);
+        }
+    }
+
+    void spoilMixture()
+    {
+        // play spoil sound effect
+        isSpoiled = true;
+    }
+
+    public void dumpMixture()
+    {
+        currentIngredients.Clear();
+        isSpoiled = false;
+    }
 }
